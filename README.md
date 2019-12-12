424fd8a39512c69d020d995bc1dfe60d74ac461f
Uma Maheswari Chandrabose <39375672+UmaMaheswariChandrabose@users.noreply.github.com>
2019-12-12 17:00:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# add-and-remove-items-in-vue-dropdown-list
-A quick start Vue project that helps you to learn how to add and remove the items dynamically to and from the Vue DropdownList of Syncfusion. You will also learn how to clear the selected item on the Vue DropdownList.
+# How to Add and Remove Items Dynamically in Syncfusion Vue DropDownList
+
+See how easily you can add and remove items dynamically from Vue Dropdown List.  You will learn how to add new items to the Vue Dropdown List pop-up by specifying an index and without an index. You will also see how to reset a selected item programmatically in the Dropdown List component, and how to remove items from the Dropdown List pop-up.  
+
+Documentatio Link: https://ej2.syncfusion.com/vue/documentation/drop-down-list/how-to/add-item/ 
+
+
+## Project pre-requisites
+Make sure that you have the compatible versions of Node and Vue-Cli in your machine before starting to work on this project.
+
+## How to run this application?
+To run this application, you need to first clone the `add-and-remove-items-dynamically-in-vue-dropdown-list` repository and then navigate to its appropriate path where it has been located in your system.
+
+To do so, open the command prompt and run the below commands one after the other.
+
+```
+git clone https://github.com/SyncfusionExamples/add-and-remove-items-dynamically-in-vue-dropdown-list dropdownlist-component
+cd dropdownlist-component
+```
+
+## Installing
+Once done with downloading, next you need to install the necessary packages required to run this application locally. The `npm install` command will install all the needed Vue packages into your current project and to do so, run the below command.
+
+```
+npm install
+```
+
+## Running on development server
+Run `npm run serve` command for a dev server. Navigate to `http://localhost:8080/`. The app will automatically reload if you change any of the source files.
+
+## Further help
+
+To get more help on the vue CLI use go check out the [Vue-Cli README](https://github.com/vuejs/vue-cli/blob/master/README.md).
---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="controls">
+      <div class="dropdownlist">
+        <br>
+        <ejs-dropdownlist  ref='dropdownObj' popupHeight="200px" popupWidth="250px" 
+        :dataSource='dataItem' :fields='dataFields' 
+        placeholder='Select a game'>
+        </ejs-dropdownlist>
+      </div>
+      <div id="button-controls">
+        <div class="button_add">
+          <ejs-button :isPrimary="true" v-on:click.native="addItems">
+          Add</ejs-button>
+        </div>
+        <div class="button_clear">
+          <ejs-button :isPrimary="true" v-on:click.native="clearText">
+          Clear</ejs-button>
+        </div>
+        <div class="button_remove">
+          <ejs-button :isPrimary="true" v-on:click.native="removeItems">
+          Remove</ejs-button>
+        </div>  
+      </div>    
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { DropDownListPlugin } from "@syncfusion/ej2-vue-dropdowns";
+Vue.use(DropDownListPlugin);
+import { ButtonPlugin } from "@syncfusion/ej2-vue-buttons";
+Vue.use(ButtonPlugin);
+
+export default Vue.extend({
+  data: function() {
+    return {
+        dataItem: [
+          { Id: 'game1', Game: 'Baseball' },
+          { Id: 'game2', Game: 'Basketball' },
+          { Id: 'game3', Game: 'Volleyball' }
+        ],
+      dataFields: { text: 'Game', value: 'Id' },
+    };
+  },
+  methods: {
+    addItems: function(){
+       this.$refs.dropdownObj.addItem({Id: 'game4', Game: 'Football'}, 0);
+    },
+    clearText: function () {
+      this.$refs.dropdownObj.ej2Instances.value = null;
+      this.$refs.dropdownObj.dataBind();
+    },
+    removeItems: function() {
+      if(this.$refs.dropdownObj.getItems().length > 1) {
+        this.$refs.dropdownObj.getItems()[0].remove();
+        this.$refs.dropdownObj.dataSource.splice(0, 1);
+        } 
+    } 
+  }
+});
+
+</script>
+<style>
+@import url(https://cdn.syncfusion.com/ej2/material.css);
+
+#button-controls {
+  display: inline-block;
+  margin-top: 17%;
+}
+.controls {
+  display: inline-block;
+  width: 100%;
+}
+.dropdownlist {
+ width: 250px;
+ margin-top: 15%;
+ margin-bottom: 15%;
+ margin-left: 25%;
+ margin-right: 1%;
+ float: left;
+}
+.button_add {
+float: left;
+padding-left: 10px;
+padding-right: 10px;
+}
+.button_clear {
+float: left;
+padding-left: 10px;
+padding-right: 10px;
+}
+.button_remove {
+float: right;
+padding-left: 10px;
+padding-right: 10px;
+}
+</style>
